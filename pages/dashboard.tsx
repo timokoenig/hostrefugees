ad82972c0fd38413b9900ea4676059e3ec061400
Timo Koenig <timo@timokoenig.com>
2022-03-15 00:22:14 +0100

    Add dashboard page

---
+++ pages/dashboard.tsx
@@ -0,0 +1,25 @@
+import { Text } from '@chakra-ui/react'
+import Layout from 'components/layout'
+import Spacer from 'components/spacer'
+import Head from 'next/head'
+import React from 'react'
+import Footer from '../components/footer'
+import Navigation from '../components/navigation'
+
+const DashboardPage = () => {
+  return (
+    <>
+      <Head>
+        <title>HostRefugees</title>
+      </Head>
+      <Layout>
+        <Navigation />
+        <Text>Dashboard</Text>
+        <Spacer />
+        <Footer />
+      </Layout>
+    </>
+  )
+}
+
+export default DashboardPage
