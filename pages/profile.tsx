f027ad432bfc7c0e1b234ec29d7c4a609177d8aa
Timo Koenig <timo@timokoenig.com>
2022-03-15 01:50:23 +0100

    Add basic information to profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,8 +1,8 @@
-import { Text } from '@chakra-ui/react'
+import { Container, FormControl, FormLabel, Heading, Input } from '@chakra-ui/react'
 import Layout from 'components/layout'
 import Spacer from 'components/spacer'
 import Head from 'next/head'
-import React from 'react'
+import React, { useState } from 'react'
 import { User } from 'utils/model'
 import { withSessionSsr } from 'utils/session'
 import Footer from '../components/footer'
@@ -13,6 +13,8 @@ type Props = {
 }
 
 const ProfilePage = (props: Props) => {
+  const [firstname, setFirstname] = useState<string>(props.user?.firstname ?? '')
+  const [lastname, setLastname] = useState<string>(props.user?.lastname ?? '')
   return (
     <>
       <Head>
@@ -20,7 +22,35 @@ const ProfilePage = (props: Props) => {
       </Head>
       <Layout>
         <Navigation user={props.user} />
-        <Text>Profile</Text>
+        <Container maxW="7xl">
+          <Heading as="h2" size="xl" mb="5">
+            Profile
+          </Heading>
+          <FormControl mb="5">
+            <FormLabel htmlFor="firstname">First Name</FormLabel>
+            <Input
+              id="firstname"
+              type="firstname"
+              value={firstname}
+              isDisabled={true}
+              onChange={e => setFirstname(e.target.value)}
+            />
+          </FormControl>
+          <FormControl mb="5">
+            <FormLabel htmlFor="lastname">Last Name</FormLabel>
+            <Input
+              id="lastname"
+              type="lastname"
+              value={lastname}
+              isDisabled={true}
+              onChange={e => setLastname(e.target.value)}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input id="email" type="email" value={props.user?.email ?? ''} isDisabled={true} />
+          </FormControl>
+        </Container>
         <Spacer />
         <Footer />
       </Layout>
