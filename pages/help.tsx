ae3844175672a33fb2d4a1df66bdd78d0f0ee4ca
Timo Koenig <timo@timokoenig.com>
2022-03-15 09:11:18 +0100

    Add help page

---
+++ pages/help.tsx
@@ -0,0 +1,35 @@
+import { Container, Text } from '@chakra-ui/react'
+import React from 'react'
+import { User } from 'utils/model'
+import { withSessionSsr } from 'utils/session'
+import Footer from '../components/footer'
+import Layout from '../components/layout'
+import Navigation from '../components/navigation'
+import Spacer from '../components/spacer'
+
+type Props = {
+  user?: User
+}
+
+const HelpPage = (props: Props) => {
+  return (
+    <Layout>
+      <Navigation user={props.user} />
+      <Container maxW="7xl">
+        <Text>Help</Text>
+      </Container>
+      <Spacer />
+      <Footer />
+    </Layout>
+  )
+}
+
+export const getServerSideProps = withSessionSsr(async function getServerSideProps(context) {
+  return {
+    props: {
+      user: context.req.session.user ?? null,
+    },
+  }
+})
+
+export default HelpPage
