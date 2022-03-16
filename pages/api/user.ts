ce4e07d2fd351751ed974fcfd5d93849441f1bce
Timo Koenig <timo@timokoenig.com>
2022-03-16 16:49:31 +0100

    Add user registration endpoint

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -6,15 +6,26 @@ type Response = {
   message?: string
 }
 
+async function handleDelete(req: NextApiRequest, res: NextApiResponse<Response>) {
+  // TODO implement delete user
+  res.send({ ok: true })
+}
+
+async function handleRegistration(req: NextApiRequest, res: NextApiResponse<Response>) {
+  // TODO implement user registration
+  res.send({ ok: true })
+}
+
 async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
-  if (req.method !== 'DELETE') {
-    res.status(400).send({ ok: false, message: 'Only DELETE requests allowed' })
+  if (req.method === 'DELETE') {
+    await handleDelete(req, res)
     return
   }
-
-  // TODO implement delete user
-
-  res.send({ ok: true })
+  if (req.method === 'POST') {
+    await handleRegistration(req, res)
+    return
+  }
+  res.status(400).send({ ok: false, message: 'Request method not allowed' })
 }
 
 export default withSessionRoute(handler)
