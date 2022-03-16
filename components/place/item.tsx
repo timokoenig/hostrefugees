796b6d9d23f91798c78d9157ad2a596e2b8dc234
Timo Koenig <timo@timokoenig.com>
2022-03-16 22:24:48 +0100

    Add image to place list item

--- components/place/item.tsx
+++ components/place/item.tsx
@@ -1,4 +1,4 @@
-import { Box, ListItem } from '@chakra-ui/react'
+import { Box, GridItem, Image, ListItem, SimpleGrid } from '@chakra-ui/react'
 import moment from 'moment'
 import { useRouter } from 'next/router'
 import React from 'react'
@@ -20,27 +20,40 @@ const PlaceItem = (props: Props) => {
         cursor="pointer"
         _hover={{ background: 'gray.100' }}
       >
-        <Box p="6">
-          <Box display="flex" alignItems="baseline">
-            <Box
-              color="gray.500"
-              fontWeight="semibold"
-              letterSpacing="wide"
-              fontSize="xs"
-              textTransform="uppercase"
-            >
-              {props.place.rooms} rooms &bull; {props.place.beds} beds
+        <SimpleGrid columns={4}>
+          <GridItem>
+            <Image
+              rounded="md"
+              alt="product image"
+              src="https://picsum.photos/900/600"
+              fit="cover"
+              align="center"
+              w="100%"
+              h="100%"
+            />
+          </GridItem>
+          <GridItem colSpan={3} p="6">
+            <Box display="flex" alignItems="baseline">
+              <Box
+                color="gray.500"
+                fontWeight="semibold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+              >
+                {props.place.rooms} rooms &bull; {props.place.beds} beds
+              </Box>
             </Box>
-          </Box>
-          <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
-            {props.place.addressCity}: {props.place.title}
-          </Box>
-          <Box>
-            {moment(props.place.availabilityStart).isBefore(moment())
-              ? 'Available Now'
-              : moment(props.place.availabilityStart).format('DD.MM.YYYY')}
-          </Box>
-        </Box>
+            <Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" isTruncated>
+              {props.place.addressCity}: {props.place.title}
+            </Box>
+            <Box>
+              {moment(props.place.availabilityStart).isBefore(moment())
+                ? 'Available Now'
+                : moment(props.place.availabilityStart).format('DD.MM.YYYY')}
+            </Box>
+          </GridItem>
+        </SimpleGrid>
       </Box>
     </ListItem>
   )
