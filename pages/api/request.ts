be8627f6d4cd875fdd9eedece14a71faf9646f97
Timo Koenig <timo@timokoenig.com>
2022-03-16 16:51:00 +0100

    Add new request endpoint

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -13,15 +13,26 @@ type Response = {
   message?: string
 }
 
+async function handleNewRequest(req: NextApiRequest, res: NextApiResponse<Response>) {
+  // TODO implement new request
+  res.send({ ok: true })
+}
+
+async function handleUpdateRequest(req: NextApiRequest, res: NextApiResponse<Response>) {
+  // TODO implement request update
+  res.send({ ok: true })
+}
+
 async function handler(req: Request, res: NextApiResponse<Response>) {
-  if (req.method !== 'PUT') {
-    res.status(400).send({ ok: false, message: 'Only PUT requests allowed' })
+  if (req.method === 'POST') {
+    await handleNewRequest(req, res)
     return
   }
-
-  // TODO implement request update
-
-  res.send({ ok: true })
+  if (req.method === 'PUT') {
+    await handleUpdateRequest(req, res)
+    return
+  }
+  res.status(400).send({ ok: false, message: 'Request method not allowed' })
 }
 
 export default withSessionRoute(handler)
