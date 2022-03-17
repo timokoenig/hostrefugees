28340a2d9c07a5f19939e090b509cfaa2ad3ad3c
Timo Koenig <timo@timokoenig.com>
2022-03-17 23:04:55 +0100

    Remove delete account functionality for now

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -6,21 +6,12 @@ type Response = {
   message?: string
 }
 
-async function handleDelete(req: NextApiRequest, res: NextApiResponse<Response>) {
-  // TODO implement delete user
-  res.send({ ok: true })
-}
-
 async function handleRegistration(req: NextApiRequest, res: NextApiResponse<Response>) {
   // TODO implement user registration
   res.send({ ok: true })
 }
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Response>) {
-  if (req.method === 'DELETE') {
-    await handleDelete(req, res)
-    return
-  }
   if (req.method === 'POST') {
     await handleRegistration(req, res)
     return
