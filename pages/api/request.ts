8744a476e7daddcda940cee9856a9989c06e6e9a
Timo Koenig <timo@timokoenig.com>
2022-03-18 01:08:36 +0100

    Add create and update request functionality

--- pages/api/request.ts
+++ pages/api/request.ts
@@ -1,31 +1,100 @@
-import { RequestStatus } from '@prisma/client'
+import { Request, RequestStatus, UserRole } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from 'prisma/client'
 import { withSessionRoute } from 'utils/session'
 
-interface Request extends NextApiRequest {
+interface CreateRequest extends NextApiRequest {
   body: {
+    request: Request
+  }
+}
+
+interface UpdateRequest extends NextApiRequest {
+  body: {
+    id: string
     status: RequestStatus
   }
 }
 
-type Response = {
-  ok: boolean
-  message?: string
+async function handleNewRequest(req: CreateRequest, res: NextApiResponse) {
+  if (req.session.user?.role !== UserRole.GUEST) {
+    // Only guests can create requests
+    res.status(400)
+    return
+  }
+
+  await prisma.request.create({
+    data: {
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      author: {
+        connect: {
+          id: req.session.user.id,
+        },
+      },
+      place: {
+        connect: {
+          id: req.body.request.placeId,
+        },
+      },
+      adults: req.body.request.adults,
+      children: req.body.request.children,
+      startDate: req.body.request.startDate,
+      endDate: req.body.request.endDate,
+      about: req.body.request.about,
+    },
+  })
+
+  // send email to host
+
+  res.status(200)
 }
 
-async function handleNewRequest(req: NextApiRequest, res: NextApiResponse<Response>) {
-  // TODO implement new request
-  res.send({ ok: true })
+async function handleUpdateRequest(req: UpdateRequest, res: NextApiResponse) {
+  const request = await prisma.request.findUnique({
+    where: {
+      id: req.body.id,
+    },
+    include: {
+      author: true,
+      place: {
+        include: {
+          author: true,
+        },
+      },
+    },
+  })
+  if (request === null) {
+    res.status(400)
+    return
+  }
+  if (
+    request.author.id !== req.session.user?.id &&
+    request.place.author.id !== req.session.user?.id
+  ) {
+    // user must be author of request (GUEST) or author of place (HOST) to update the status
+    res.status(400)
+    return
+  }
+
+  await prisma.request.update({
+    where: {
+      id: req.body.id,
+    },
+    data: {
+      updatedAt: new Date(),
+      status: req.body.status,
+    },
+  })
+
+  // TODO send emails to both parties
+
+  res.status(200)
 }
 
-async function handleUpdateRequest(req: NextApiRequest, res: NextApiResponse<Response>) {
-  // TODO implement request update
-  res.send({ ok: true })
-}
-
-async function handler(req: Request, res: NextApiResponse<Response>) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.session.user == undefined) {
-    res.status(401).send({ ok: false, message: 'Not allowed' })
+    res.status(401)
     return
   }
   if (req.method === 'POST') {
@@ -36,7 +105,7 @@ async function handler(req: Request, res: NextApiResponse<Response>) {
     await handleUpdateRequest(req, res)
     return
   }
-  res.status(400).send({ ok: false, message: 'Request method not allowed' })
+  res.status(400)
 }
 
 export default withSessionRoute(handler)
