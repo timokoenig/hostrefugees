c53dd7a92ce852d57e88e8cdf43b81dba0558b9f
Timo Koenig <timo@timokoenig.com>
2022-03-21 00:59:47 +0100

    Remove PWA because we don't need it

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-    dest: 'public',
-  },
+module.exports = {
   trailingSlash: true,
   experimental: {
     outputStandalone: true,
   },
-})
+}
