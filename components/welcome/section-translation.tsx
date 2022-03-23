99ac73c1718d27e24748d4e7def805c86c2ad328
Timo Koenig <timo@timokoenig.com>
2022-03-23 13:14:59 +0100

    Add additional phrases and words for welcome page

--- components/welcome/section-translation.tsx
+++ components/welcome/section-translation.tsx
@@ -14,17 +14,28 @@ import React from 'react'
 import { IoChatbubblesSharp } from 'react-icons/io5'
 import Section from './section'
 
-const translations: { de: string; ua: string }[] = [
+const translationsWords: { de: string; ua: string }[] = [
   { de: 'Hallo', ua: 'Здравствуйте' },
-  { de: 'Ich spreche kein Deutsch', ua: 'Я не розмовляю німецькою' },
-  { de: 'Sprichst du Englisch?', ua: 'Ви розмовляєте англійською?' },
-  { de: 'Wo ist der nächste Supermarkt?', ua: 'Де наступний супермаркет?' },
-  { de: 'Ich suche ...', ua: 'Я шукаю' },
-  { de: 'Krankenhaus', ua: 'лікарні' },
+  { de: 'Bitte', ua: 'Будь ласка' },
+  { de: 'Danke', ua: 'Спасибі' },
+  { de: 'Tschüss', ua: 'до побачення' },
+  { de: 'Krankenhaus', ua: 'Лікарня' },
   { de: 'Arzt', ua: 'лікар' },
   { de: 'Apotheke', ua: 'аптека' },
   { de: 'Amt', ua: 'урядова установа' },
   { de: 'Hilfe', ua: 'допомогти' },
+  { de: 'Polizei', ua: 'Поліція' },
+  { de: 'Feuerwehr', ua: 'Пожежне депо' },
+]
+
+const translationsPhrases: { de: string; ua: string }[] = [
+  { de: 'Können Sie mir helfen?', ua: 'Можеш допомогти мені?' },
+  { de: 'Ich spreche kein Deutsch', ua: 'Я не розмовляю німецькою' },
+  { de: 'Sprechen Sie Englisch?', ua: 'Ви розмовляєте англійською?' },
+  { de: 'Wo finde ich... den Bahnhof?', ua: 'Де я можу знайти... залізнична станція?' },
+  { de: 'Wo finde ich... eine Toilette?', ua: 'Де я можу знайти... туалет?' },
+  { de: 'Wo ist der nächste Supermarkt?', ua: 'Де наступний супермаркет?' },
+  { de: 'Ich habe mich verlaufen', ua: 'Я заблукав' },
 ]
 
 const SectionTranslations = () => {
@@ -43,7 +54,7 @@ const SectionTranslations = () => {
               </Tr>
             </Thead>
             <Tbody>
-              {translations.map(trans => (
+              {translationsWords.map(trans => (
                 <Tr key={trans.de}>
                   <Td>{trans.de}</Td>
                   <Td>{trans.ua}</Td>
@@ -61,7 +72,7 @@ const SectionTranslations = () => {
               </Tr>
             </Thead>
             <Tbody>
-              {translations.map(trans => (
+              {translationsPhrases.map(trans => (
                 <Tr key={trans.de}>
                   <Td>{trans.de}</Td>
                   <Td>{trans.ua}</Td>
