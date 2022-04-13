922ac1d992ce899861d8814a6280d78f2e53088b
Timo Koenig <timo@timokoenig.com>
2022-04-13 16:13:17 +0200

    Fix place slice on index page

--- components/map-places.tsx
+++ components/map-places.tsx
@@ -30,7 +30,7 @@ const MapPlaces = (props: Props) => {
                 {t('place.empty')}
               </Text>
             )}
-            {props.places.slice(3).map((place, i) => (
+            {props.places.slice(0, 4).map((place, i) => (
               <PlaceItem key={i} place={place} />
             ))}
             {props.places.length > 0 && <MoreItem />}
