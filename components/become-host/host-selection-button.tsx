4c07a692fc25cace17399542efc82d04204334b7
Timo Koenig <timo@timokoenig.com>
2022-04-13 13:14:45 +0200

    Fix dark mode for become host type selection

--- components/become-host/host-selection-button.tsx
+++ components/become-host/host-selection-button.tsx
@@ -1,4 +1,4 @@
-import { Button, Image } from '@chakra-ui/react'
+import { Button, Image, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 
 type Props = {
@@ -8,28 +8,31 @@ type Props = {
   onClick: () => void
 }
 
-const HostSelectionButton = (props: Props) => (
-  <Button
-    maxWidth="300px"
-    height="auto"
-    rounded="10"
-    bg={props.selected ? undefined : 'white'}
-    colorScheme={props.selected ? 'blue' : undefined}
-    borderColor="gray.100"
-    borderWidth="1px"
-    textTransform="uppercase"
-    fontSize="xl"
-    p="10"
-    flexDirection="column"
-    _hover={{
-      transform: 'translateY(2px)',
-      boxShadow: 'lg',
-    }}
-    onClick={props.onClick}
-  >
-    <Image width="100%" src={props.image} mb="10" />
-    {props.title}
-  </Button>
-)
+const HostSelectionButton = (props: Props) => {
+  const backgroundColor = useColorModeValue('white', 'gray.800')
+  return (
+    <Button
+      maxWidth="300px"
+      height="auto"
+      rounded="10"
+      bg={props.selected ? undefined : backgroundColor}
+      colorScheme={props.selected ? 'blue' : undefined}
+      borderColor="gray.100"
+      borderWidth="1px"
+      textTransform="uppercase"
+      fontSize="xl"
+      p="10"
+      flexDirection="column"
+      _hover={{
+        transform: 'translateY(2px)',
+        boxShadow: 'lg',
+      }}
+      onClick={props.onClick}
+    >
+      <Image width="100%" src={props.image} mb="10" />
+      {props.title}
+    </Button>
+  )
+}
 
 export default HostSelectionButton
